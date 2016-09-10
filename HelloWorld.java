adcfdd180c179e21935671c6e5de2a2d855c4eee
SACCS112MartinezIrvin <marsplanet1995@gmail.com>
2016-09-09 20:33:32 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+
+/**
+
+ * File name: HelloWorld.java  
+ * The HelloWorld Program for cs112 
+ * Email to huynh_durendal@sac.edu
+ * The HelloWorld Program displays Hello World! on the screen
+
+ */ 
+
+  public class HelloWorld
+   {
+
+ 	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+
+   } 
